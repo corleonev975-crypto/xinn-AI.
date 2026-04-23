1372323cc30eb81ba80fac636380bfc003d5c524
corleonev975-crypto <corleonev975@gmail.com>
2026-04-24 05:31:40 +0700

    Update avatar.tsx

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,11 +1,5 @@
-import Image from "next/image";
+"use client";
 
-export function XinnAvatar({ size = 58 }: { size?: number }) {
-  return (
-    <div className="avatar-wrap" style={{ width: size, height: size }}>
-      <div className="avatar-inner">
-        <Image src="/xinn-avatar.gif" alt="XINN AI" width={size} height={size} priority />
-      </div>
-    </div>
-  );
+export default function XinnAvatar() {
+  return <div className="avatar">X</div>;
 }
